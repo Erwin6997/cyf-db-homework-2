4bf602cb9f0db43bb4028a9303c128437730df80
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-07-25 17:55:28 +0100

    f

--- server.js
+++ server.js
@@ -1,29 +1,107 @@
-const dotenv = require('dotenv')
-const express = require('express')
-const mongodb = require('mongodb')
+const dotenv = require('dotenv');
+const express = require('express');
+const mongodb = require('mongodb');
+const { getPutBodyIsAllowed } = require('./util');
 
-const { getPutBodyIsAllowed } = require('./util')
+dotenv.config();
 
-dotenv.config()
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
-const app = express()
-app.use(express.json())
-
-const port = process.env.PORT || 3000
-
-const uri = process.env.DATABASE_URI
+const uri = process.env.DATABASE_URI;
 
 app.post('/api/books', function(request, response) {
+  const client = new mongodb.MongoClient(uri);
+    
+  const title = request.query.title;
+  const author = request.query.author;
+  const author_birth_year = request.query.author_birth_year;
+  const author_death_year = request.query.author_death_year;
+  const url = request.query.url;
+
+    
+    client.connect(() => {
+      const db = client.db('literature')
+      const collection = db.collection('books')
+        const book = {
+          title: title,
+          author: author,
+          author_birth_year: author_birth_year,
+          author_death_year: author_death_year,
+          url: url
+      };
+        collection.insertOne(book, function (error, result) {
+            if(error){
+            response.sendStatus(400).send(error);
+            }else {
+                response.sendStatus(200).send(result.ops[0]);
+            }
+            client.close();
+        });
+    });
+        
+    console.log("ADD NEW Films:", request.query);
   // Make this work!
-})
+});
 
 app.delete('/api/books/:id', function(request, response) {
+  const client = new mongodb.MongoClient(uri);
+
+    const searchObject = { "id" : request.params.id };
+
+    console.log(searchObject);
+    client.connect(() => {
+      const db = client.db('literature')
+      const collection = db.collection('books')
+    collection.deleteOne(searchObject , function (error, result) {
+        if(error){
+          request.sendStatus(400).send(error);
+            }else if (request.result) {
+              request.sendStatus(204)
+            } else {
+              request.sendStatus(404)
+            }
+            client.close();
+        });
+    });
   // Make this work, too!
-})
+});
 
 app.put('/api/books/:id', function(request, response) {
+  const client = new mongodb.MongoClient(uri);
+
+  const searchObject = { "id" : request.params.id };
+  console.log("hiiiiiiii");
+  console.log(searchObject);
+  client.connect(() => {
+    const db = client.db('literature')
+    const collection = db.collection('books')
+    collection.find(searchObject).toArray(function(error, books) {
+      response.send(error || books);
+      const update = {
+        $set: {
+          title,
+          author,
+          author_birth_year,
+          author_death_year,
+          url,
+        },
+      };
+  collection.findOneAndUpdate(searchObject , update ,function (error, result) {
+      if(error){
+        request.sendStatus(400).send(error);
+          }else if (request.result) {
+            request.sendStatus(204)
+          } else {
+            request.sendStatus(404)
+          }
+          client.close();
+      });
+    });
   // Also make this work!
-})
+  });
+});
 
 app.get('/api/books', function(request, response) {
   const client = new mongodb.MongoClient(uri)
@@ -45,8 +123,8 @@ app.get('/api/books', function(request, response) {
     collection.find(searchObject).toArray(function(error, books) {
       response.send(error || books)
       client.close()
-    })
-  })
+    });
+  });
 })
 
 app.get('/api/books/:id', function(request, response) {
@@ -74,29 +152,29 @@ app.get('/api/books/:id', function(request, response) {
       }
 
       client.close()
-    })
-  })
-})
+    });
+  });
+});
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html')
-})
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + '/index.html')
+// })
 
-app.get('/books/new', function(request, response) {
-  response.sendFile(__dirname + '/new-book.html')
-})
+// app.get('/books/new', function(request, response) {
+//   response.sendFile(__dirname + '/new-book.html')
+// })
 
-app.get('/books/:id', function(request, response) {
-  response.sendFile(__dirname + '/book.html')
-})
+// app.get('/books/:id', function(request, response) {
+//   response.sendFile(__dirname + '/book.html')
+// })
 
-app.get('/books/:id/edit', function(request, response) {
-  response.sendFile(__dirname + '/edit-book.html')
-})
+// app.get('/books/:id/edit', function(request, response) {
+//   response.sendFile(__dirname + '/edit-book.html')
+// })
 
-app.get('/authors/:name', function(request, response) {
-  response.sendFile(__dirname + '/author.html')
-})
+// app.get('/authors/:name', function(request, response) {
+//   response.sendFile(__dirname + '/author.html')
+// })
 
 app.listen(port || 3000, function() {
   console.log(`Running at \`http://localhost:${port}\`...`)
